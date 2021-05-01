1bc5095b0bf9d3519af0d6eb30b6b5b495257663
nvCuong <nguyenvancuong215@gmail.com>
2021-05-01 22:23:59 +0700

    firebase

---
+++ firebase-demo/script.js
@@ -0,0 +1,242 @@
+let data = [
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12_2__3.jpg",
+    discount: "-15%",
+    name: "iPhone 12 I Chính hãng VN/A  AA",
+    new_price: "19.890.000đ",
+    old_price: "22.990.000đ",
+    rating: 5,
+    evaluate: "10 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm ipad pro",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+  {
+    link: "https://cellphones.com.vn/iphone-12-pro-max.html",
+    img_url:
+      "https://cdn.cellphones.com.vn/media/catalog/product/cache/1/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
+    discount: "-13%",
+    name: "iPhone 12 Pro Max I Chính hãng VN/A",
+    new_price: "29.299.000đ",
+    old_price: "32.990.000đ",
+    rating: 4,
+    evaluate: "160 đánh giá",
+    promotion: "Giảm thêm 500.000đ khi mua kèm apple watch series 6",
+  },
+];
+
+let fakeData = {
+  data: [1, 2, 3],
+};
+
+let getData = async () => {
+  let result = await firebase.firestore().collection("demone").get();
+  console.log(result);
+  let data = await getDataFromDocs(result.docs);
+  console.log(data);
+};
+
+getData();
+
+let createData = async (d) => {
+  await firebase.firestore().collection("demone").add(d);
+};
+// createData(fakeData)
+
+let addData = async () => {
+    let updateNe = {
+        name: "cuong",
+        age: 96
+    }
+  await firebase
+    .firestore()
+    .collection("demone")
+    .doc("Md3UZsdmpWvcWFCNMUtr")
+    .update({
+        students : firebase.firestore.FieldValue.arrayUnion(updateNe)
+    });
+};
+// addData()
+
+let updateData = async () => {
+    let datamoiNe = {
+        name: "Minh",
+        age: 96
+    }
+  await firebase
+    .firestore()
+    .collection("demone")
+    .doc("Md3UZsdmpWvcWFCNMUtr")
+    .set(datamoiNe)
+};
+updateData()
+
+let getDataFromDoc = (doc) => {
+  let data = doc.data();
+  data.id = doc.id;
+  return data;
+};
+let getDataFromDocs = (docs) => {
+  let result = [];
+  for (let doc of docs) {
+    let data = getDataFromDoc(doc);
+    result.push(data);
+  }
+  return result;
+};
